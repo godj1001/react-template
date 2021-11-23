4618c02dbca4ebbdfbbe9d1170b93dc5172376a3
zhangjun55 <zhangjun55@baidu.com>
2021-11-23 17:15:46 +0800

    update: add groove monitor

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import Monitor from 'groove-monitor'
 console.log('May you have a good experience in Groove react template');
+new Monitor()
 
 ReactDOM.render(<App />,document.getElementById('root'))
