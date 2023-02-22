3c3c2b2d0eea2af577cd0caa97ad1f1128e241d0
zhangjun.groove <zhangjun.groove@bytedance.com>
2023-02-22 09:31:47 +0800

    feat: 更新配置项

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
-import * as React from 'react';
-import Style from './App.module.scss';
+import React from "react";
+import "./index.css";
+import Style from "./App.module.scss";
 const App = () => {
-    return (
-        <div className={Style.App} id='wrapper'>
-            <div className={Style.box1}>
-            </div>
-            <div className={Style.box2}></div>
-            <div className={Style.title}>
-                <span className={Style.firstWord}>
-                    G
-                </span>
-                <span className={Style.other}>
-                    roove
-                </span>
-            </div>
-        </div>
-    );
+  return (
+    <div className={Style.App} id="wrapper">
+      <div className={Style.box1}></div>
+      <div className={Style.box2}></div>
+      <div className={Style.title}>
+        <span className={Style.firstWord}>G</span>
+        <span className={Style.other}>roove</span>
+      </div>
+    </div>
+  );
 };
 
 export default App;
